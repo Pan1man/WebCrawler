c04c351f3aa64c5de94067894676839def3d3874
Alexandr Gruzdev <testWorkmail@gmail.com>
2024-02-20 15:08:21 +0300

    small fixes

--- src/api/routers.py
+++ src/api/routers.py
@@ -1,4 +1,7 @@
 from fastapi import APIRouter
+from typing import List
+
+from src.models.web_pages import WebPage
 from src.utils.repository import WebPageRepository
 
 router = APIRouter(
@@ -7,6 +10,6 @@ router = APIRouter(
 
 @router.get("")
 async def get_pages():
-    #import pdb; pdb.set_trace()
     pages = await WebPageRepository.get_all()
+
     return {"pages": pages}
--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from src.api.routers import router as page_router
 
@@ -6,3 +7,5 @@ app = FastAPI()
 app.include_router(page_router)
 
 
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
