16c626890af7a9f5e15fc8c2b2b2f39062461d70
Alexandr Gruzdev <testWorkmail@gmail.com>
2024-02-27 20:51:06 +0300

    done creating creation

--- src/api/pages.py
+++ src/api/pages.py
@@ -1,8 +1,6 @@
-
 from fastapi import APIRouter, Depends
-from sqlalchemy import select
+from sqlalchemy import select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
-from typing_extensions import List
 
 from src.db.db import get_async_session
 from src.models.pages import Page
@@ -18,3 +16,9 @@ async def get_pages(session: AsyncSession = Depends(get_async_session)):
     result = pages.scalars().all()
     return result
 
+
+@router.put("/add_page")
+async def add_page(page: PageBase, session: AsyncSession = Depends(get_async_session)):
+    stmt = insert(Page).values(**page.dict())
+    await session.execute(stmt)
+    await session.commit()
